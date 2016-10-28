26e24d0838285ce9e2ccc447dff8c837a4b986fd
zhanghuan0723 <simon0723@126.com>
2016-10-28 21:43:10 +0800

    1.3 添加Rxjava

--- app/src/main/java/com/zheling/MainActivity.java
+++ app/src/main/java/com/zheling/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zheling.entity.MovieEntity;
 import com.zheling.http.MovieService;
@@ -11,11 +12,12 @@ import com.zheling.http.MovieService;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,20 +45,28 @@ public class MainActivity extends AppCompatActivity {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
 
         MovieService movieService = retrofit.create(MovieService.class);
-        Call<MovieEntity> call = movieService.getTopMovie(0, 10);
-        call.enqueue(new Callback<MovieEntity>() {
-            @Override
-            public void onResponse(Call<MovieEntity> call, Response<MovieEntity> response) {
-                resultTV.setText(response.body().toString());
-            }
+        movieService.getTopMovie(0, 10)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<MovieEntity>() {
+                    @Override
+                    public void onCompleted() {
+                        Toast.makeText(MainActivity.this, "Get Top Movie Completed", Toast.LENGTH_LONG).show();
+                    }
 
-            @Override
-            public void onFailure(Call<MovieEntity> call, Throwable t) {
-                resultTV.setText(t.getMessage());
-            }
-        });
+                    @Override
+                    public void onError(Throwable e) {
+                        resultTV.setText(e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(MovieEntity movieEntity) {
+                        resultTV.setText(movieEntity.toString());
+                    }
+                });
     }
 }
--- app/src/main/java/com/zheling/http/MovieService.java
+++ app/src/main/java/com/zheling/http/MovieService.java
@@ -2,14 +2,14 @@ package com.zheling.http;
 
 import com.zheling.entity.MovieEntity;
 
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by Simon on 2016-10-28.
  */
 public interface MovieService {
     @GET("top250")
-    Call<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
+    Observable<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
 }
