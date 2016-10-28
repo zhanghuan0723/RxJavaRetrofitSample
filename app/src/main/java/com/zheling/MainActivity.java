e849a709618cfd551240352f2a8f5d7f583d7add
zhanghuan0723 <simon0723@126.com>
2016-10-28 21:09:46 +0800

    1.1 基本页面

--- app/src/main/java/com/zheling/MainActivity.java
+++ app/src/main/java/com/zheling/MainActivity.java
@@ -1,13 +1,35 @@
 package com.zheling;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.click_me_BN)
+    Button clickMeBN;
+    @Bind(R.id.result_TV)
+    TextView resultTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.click_me_BN)
+    public void onClick() {
+        getMovie();
+    }
+
+    // 进行网络请求
+    private void getMovie() {
+
     }
 }
